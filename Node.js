96a89130e351cf5b6444a0920b6773036924da19
Daniel Groleau <Daniel.Groleau@arrow.com>
2021-03-25 14:33:35 -0600

    add linked list

--- Node.js
+++ Node.js
@@ -3,13 +3,15 @@ class Node {
       this.data = data;
       this.next = null;
     }
-  
     setNextNode(node) {
-    if (node instanceof Node || node === null) {
-      this.next = node;
-    } else {
-      throw new Error('error');
+      if (node instanceof Node || node === null) {
+        this.next = node;
+      } else {
+        throw new Error('Next node must be a member of the Node class.');
+      }
     }
+    getNextNode() {
+      return this.next;
     }
   }
   const firstNode = new Node('I am an instance of a Node!');
